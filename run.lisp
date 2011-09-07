4a18b02610839aacafcad347e98820bbc4ad156d
Erik Nomitch <enomitch@gmail.com>
2011-09-07 12:43:41 -0700

    gets /riak/test

--- run.lisp
+++ run.lisp
@@ -1,7 +1,21 @@
 ;; ===============================================
-;; RIAK ==========================================
+;; CL-RIAK =======================================
 ;; ===============================================
-(defpackage #:riak
+(defpackage #:cl-riak
   (:use #:cl
-        #:drakma))
+        #:drakma
+        #:flexi-streams))
 
+(in-package #:cl-riak)
+
+;; -----------------------------------------------
+;; TEST ------------------------------------------
+;; -----------------------------------------------
+;;(setq *header-stream* *standard-output*)
+;;(setf *drakma-default-external-format* :utf-8)
+
+(defvar *riak*
+  (http-request "http://127.0.0.1:8098/riak/test"))
+
+(format t "~a~%" *riak*)
+(format t "~a~%" (octets-to-string *riak*))
