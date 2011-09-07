830ba0bb404282e7e1e385464760559e4a2709ba
Erik Nomitch <enomitch@gmail.com>
2011-09-07 13:45:04 -0700

    request takes a value

--- run.lisp
+++ run.lisp
@@ -20,25 +20,56 @@
 ;; -----------------------------------------------
 ;; REQUESTS --------------------------------------
 ;; -----------------------------------------------
-(define-exported-function request-url-suffix (url-suffix)
-  (let ((http-response
-          (http-request (format nil "~a:~a~a" *riak-host* *riak-port* url-suffix))))
-    (typecase http-response
-      (string
-        http-response)
-      (vector
-        (octets-to-string http-response))
-      (otherwise
-        http-response))))
+(define-function parse-http-response (http-response)
+  (typecase http-response
+    ;; This is a normal response
+    (string
+      http-response)
+    ;; This is the JSON description thing
+    ((simple-array (unsigned-byte 8))
+      (octets-to-string http-response))
+    ;; This is unknown
+    (otherwise
+      http-response)))
 
-(define-exported-function request (bucket key)
-  (request-url-suffix (format nil "/riak/~a/~a" bucket key)))
+(define-function make-url-suffix (bucket key)
+  (format nil "/riak/~a/~a" bucket key))
+
+(define-exported-function request-url-suffix (url-suffix &key (method :get)
+                                                              (content nil))
+  (parse-http-response
+    (http-request (format nil "~a:~a~a" *riak-host* *riak-port* url-suffix)
+                  :method  method
+                  :content content)))
+
+;;   (let ((http-response
+;;           (http-request (format nil "~a:~a~a" *riak-host* *riak-port* url-suffix)
+;;                         :method     method
+;;                         :content "this is another test")))
+;;     (typecase http-response
+;;       ;; This is a response
+;;       (string
+;;         http-response)
+;;       ;; This is the JSON description thing
+;;       ((simple-array (unsigned-byte 8))
+;;         (format t "~a~%" (type-of http-response))
+;;         (octets-to-string http-response))
+;;       ;; This is unknown
+;;       (otherwise
+;;         http-response))))
+
+(define-exported-function request (bucket key &optional (value nil))
+  (if value
+    (request-url-suffix (make-url-suffix bucket key) :method :post :content value)
+    (request-url-suffix (make-url-suffix bucket key))))
 
 ;; -----------------------------------------------
 ;; TEST ------------------------------------------
 ;; -----------------------------------------------
-(defvar *test* (request "test" "foo"))
+(defvar *get* (request "test" "foo"))
+(defvar *add* (request "test" "bar" "test1"))
 
-;;   (octets-to-string 
 ;;(setq *header-stream* *standard-output*)
 ;;(setf *drakma-default-external-format* :utf-8)
+
+;;(format t "~a~%" *get*)
