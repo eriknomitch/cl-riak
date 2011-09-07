ac91bc4b33c4312a9e3e2ac81c2aad32dfc0688f
Erik Nomitch <enomitch@gmail.com>
2011-09-07 13:12:04 -0700

    request handles byte vectors and strings

--- run.lisp
+++ run.lisp
@@ -20,14 +20,25 @@
 ;; -----------------------------------------------
 ;; REQUESTS --------------------------------------
 ;; -----------------------------------------------
-(define-exported-function request (url-suffix)
-  (octets-to-string 
-    (http-request (format nil "~a:~a~a" *riak-host* *riak-port* url-suffix))))
+(define-exported-function request-url-suffix (url-suffix)
+  (let ((http-response
+          (http-request (format nil "~a:~a~a" *riak-host* *riak-port* url-suffix))))
+    (typecase http-response
+      (string
+        http-response)
+      (vector
+        (octets-to-string http-response))
+      (otherwise
+        http-response))))
+
+(define-exported-function request (bucket key)
+  (request-url-suffix (format nil "/riak/~a/~a" bucket key)))
 
 ;; -----------------------------------------------
 ;; TEST ------------------------------------------
 ;; -----------------------------------------------
-(defvar *test* (request "/riak/test"))
+(defvar *test* (request "test" "foo"))
 
+;;   (octets-to-string 
 ;;(setq *header-stream* *standard-output*)
 ;;(setf *drakma-default-external-format* :utf-8)
